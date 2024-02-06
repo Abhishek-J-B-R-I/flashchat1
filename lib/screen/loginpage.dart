aa9376e05329b37f7c1b8ab50fbec5c84f999173
Abhishek-J-B-R-I <abhishekmrjbr@outlook.com>
2024-02-07 00:07:50 +0530

    loading animation

--- lib/screen/loginpage.dart
+++ lib/screen/loginpage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled1/screen/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 class loginpage extends StatefulWidget {
 
   static String l='loginpage';
@@ -12,6 +13,7 @@ class loginpage extends StatefulWidget {
 
 class _loginpageState extends State<loginpage> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner=false;
   late String email;
   late String password;
 
@@ -19,76 +21,84 @@ class _loginpageState extends State<loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Hero(
-                  tag: 'logo',
-                  child: Image(
-                    image: AssetImage("images/logo.png"),
-                    height: 150,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: 'logo',
+                    child: Image(
+                      image: AssetImage("images/logo.png"),
+                      height: 150,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30,),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email=value;
-              },
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                    50,
-                  )),
-                  hintText: "Enter your email"),
-            ),
-            SizedBox(height: 10,),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              //keyboardType: TextInputType.,
-              onChanged: (value) {
-                password=value;
-              },
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                  hintText: "Enter your password"),
-            ),
-            SizedBox(height: 30,),
-            Hero(
-              tag: 'lg',
-              child: FilledButton(
-                onPressed: () async{
-                 try{
-                   final user= await _auth.signInWithEmailAndPassword(email: email, password: password);
-                   if(user!=null){
-                     Navigator.pushNamed(context, chat_screen.chatnow);
-                   }
-
-                 }catch(e){
-                   print(e);
-                 }
-
-                },
-                child: Text(
-                  "Log In",
-                ),
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStatePropertyAll(Colors.lightBlueAccent)),
+                ],
               ),
-            )
-          ],
+              SizedBox(height: 30,),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email=value;
+                },
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                      50,
+                    )),
+                    hintText: "Enter your email"),
+              ),
+              SizedBox(height: 10,),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                //keyboardType: TextInputType.,
+                onChanged: (value) {
+                  password=value;
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    hintText: "Enter your password"),
+              ),
+              SizedBox(height: 30,),
+              Hero(
+                tag: 'lg',
+                child: FilledButton(
+                  onPressed: () async{
+                    setState(() {
+                      showSpinner=true;
+                    });
+                   try{
+                     final user= await _auth.signInWithEmailAndPassword(email: email, password: password);
+                     if(user!=null){
+                       Navigator.pushNamed(context, chat_screen.chatnow);
+                     }
+
+                   }catch(e){
+                     print(e);
+                   }
+                    setState(() {
+                      showSpinner=false;
+                    });
+                  },
+                  child: Text(
+                    "Log In",
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.lightBlueAccent)),
+                ),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(child: Icon(Icons.home),backgroundColor: Colors.blueAccent,onPressed: (){Navigator.pop(context);},),
--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:untitled1/screen/chat_screen.dart';
-
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 class registration extends StatefulWidget {
   static String rg = 'register';
 
@@ -11,84 +11,91 @@ class registration extends StatefulWidget {
 
 class _registrationState extends State<registration> {
   final _auth = FirebaseAuth.instance;
-
+  bool showSpinner=false;
   late String email;
   late String pass;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Hero(
-                    tag: 'logo',
-                    child: Image(
-                        image: AssetImage("images/logo.png"), height: 180)),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: InputDecoration(
-                  hintText: "Enter your email",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  )),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                pass = value;
-              },
-              decoration: InputDecoration(
-                  hintText: "Enter your password",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Hero(
-              tag: 'r',
-              child: FilledButton(
-                onPressed: () async {
-                  
-                  try {
-                    final newuser = await _auth.createUserWithEmailAndPassword(email: email, password: pass);
-                    if(newuser!=null){
-                      Navigator.pushNamed(context, chat_screen.chatnow);
-                    }
-                  } catch(e){
-print(e);
-                  }
-
-                  // print(email);
-                  //print(pass);
-                },
-                child: Text("Register"),
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStatePropertyAll(Colors.blueAccent)),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                      tag: 'logo',
+                      child: Image(
+                          image: AssetImage("images/logo.png"), height: 180)),
+                ],
               ),
-            )
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(
+                    hintText: "Enter your email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  pass = value;
+                },
+                decoration: InputDecoration(
+                    hintText: "Enter your password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Hero(
+                tag: 'r',
+                child: FilledButton(
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner=true;
+                    });
+                    try {
+                      final newuser = await _auth.createUserWithEmailAndPassword(email: email, password: pass);
+                      if(newuser!=null){
+                        Navigator.pushNamed(context, chat_screen.chatnow);
+                      }
+                    } catch(e){
+                      print(e);
+                    }
+                  setState(() {
+                    showSpinner=false;
+                  });
+                    // print(email);
+                    //print(pass);
+                  },
+                  child: Text("Register"),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.blueAccent)),
+                ),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
