305c0830320fa72288ce47c0661baa7754062e78
Abhishek-J-B-R-I <abhishekmrjbr@outlook.com>
2024-02-18 17:56:02 +0530

    bubble

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'register.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 class chat_screen extends StatefulWidget {
   static String chatnow = 'chat_screen';
   //const chat_screen({super.key});
@@ -12,27 +13,25 @@ class chat_screen extends StatefulWidget {
 
 class _chat_screenState extends State<chat_screen> {
   final _auth = FirebaseAuth.instance;
-  final _firestorecloud=FirebaseFirestore.instance;
+  final _firestorecloud = FirebaseFirestore.instance;
   late String mes;
   //FirebaseUser loguser;
   late User loguser;
 
-@override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
     currenuser();
-}
+  }
 
   void currenuser() async {
     try {
-
-      final user= await _auth.currentUser;
-      if(user!=null){
-        loguser=user;
+      final user = await _auth.currentUser;
+      if (user != null) {
+        loguser = user;
         //print(loguser.email);
       }
-
     } catch (e) {
       print(e);
     }
@@ -45,82 +44,111 @@ class _chat_screenState extends State<chat_screen> {
   }
 }*/
 
-  void getdatamessageusingsnapchat()async{
-    await for(var i in  _firestorecloud.collection('messages').snapshots()){
-      for(var ms in i.docs){
+  void getdatamessageusingsnapchat() async {
+    await for (var i in _firestorecloud.collection('messages').snapshots()) {
+      for (var ms in i.docs) {
         print(ms.data());
       }
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Chat Now"),
         actions: [
-          IconButton(onPressed: (){
-              getdatamessageusingsnapchat();
-            //getdatamessage();
-            /*_auth.signOut();
+          IconButton(
+              onPressed: () {
+                getdatamessageusingsnapchat();
+                //getdatamessage();
+                /*_auth.signOut();
             Navigator.pop(context);*/
-          }, icon: Icon(Icons.close))
+              },
+              icon: Icon(Icons.close))
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-
           children: [
-            StreamBuilder(stream:  _firestorecloud.collection('messages').snapshots(), builder: (context,snapshot){
-              if(snapshot.hasData){
-                final messages = snapshot.data?.docs;
-                List<Text> mw=[];
-                for(var message in messages!){
+            StreamBuilder(
+                stream: _firestorecloud.collection('messages').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                        child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlue,
+                    ));
+                  }
+                  if (snapshot.hasData) {
+                    final messages = snapshot.data?.docs;
+                    List<mbubble> mw = [];
+                    for (var message in messages!) {
+                      final mt = message.data()['text'];
+                      final ms = message.data()['sender'];
+                      final messagebubbler = mbubble(ms: ms,mt: mt,);
+                      mw.add(messagebubbler);
+                    }
 
-                  final mt=message.data ()['text'];
-                  final ms=message.data()['sender'];
-                  final storemessage=Text('$mt from $ms');
-                  mw.add(storemessage);
-
-                }
-
-                return Column(
-                  children:
-                    mw,
-
-                );
-
-              }else{
-                throw Exception('failed to retrieve messages');
-              }
-            }),
-
-            Row(
-              children: [
-                SizedBox(
-                  width: 310,
-                  child: TextField(
-                    onChanged: (value) {
-                      mes=value;
-                    },
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: "Type your message here..."),
+                    return Expanded(
+                      child: ListView(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                        children: mw,
+                      ),
+                    );
+                  } else {
+                    throw Exception('failed to retrieve messages');
+                  }
+                }),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 310,
+                    child: TextField(
+                      onChanged: (value) {
+                        mes = value;
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          hintText: "Type your message here..."),
+                    ),
                   ),
-                ),
-                TextButton(onPressed: () {
-                  _firestorecloud.collection('messages').add({
-                    'text':mes,
-                    'sender':loguser.email,
-
-                  });
-                }, child: Text("Send"))
-              ],
+                  TextButton(
+                      onPressed: () {
+                        _firestorecloud.collection('messages').add({
+                          'text': mes,
+                          'sender': loguser.email,
+                        });
+                      },
+                      child: Text("Send"))
+                ],
+              ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class mbubble extends StatelessWidget {
+  mbubble({required this.ms,required this.mt});
+   final String? mt;
+   final String? ms;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+     color: Colors.lightBlueAccent ,
+      child: Text(
+        '$mt from $ms',
+        style: TextStyle(fontSize: 30),
       ),
     );
   }
